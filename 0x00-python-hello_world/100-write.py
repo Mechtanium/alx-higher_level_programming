c154e353578d3965c20b58c681317579b3e6b273
Mechtanium <enosmath@gmail.com>
2022-12-26 16:05:06 +0000

    Line too long fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,5 +2,6 @@
 import sys
 import os
 
-os.write(2, bytearray("and that piece of art is useful - Dora Korpar, 2015-10-19\n", encoding="UTF-8", errors="e"))
+os.write(2, bytearray("and that piece of art is useful - \
+Dora Korpar, 2015-10-19\n", encoding="UTF-8", errors="e"))
 sys.exit(1)
