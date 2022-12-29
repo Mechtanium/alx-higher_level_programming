fd6455056baf7c85172ef56cfbe80b0d61a62b32
Mechtanium <a.sapiens.view@gmail.com>
2022-12-29 02:15:11 +0100

    Print hex numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{}".format(i).zfill(2), end="")
-    if i != 99:
-        print(", ", end="")
+for i in range(99):
+    print("{}".format(i).zfill(2), ", ", end="", sep="")
+
+print("99", end="\n")
